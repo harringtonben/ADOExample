99524a0a53d05a2a1fb3f2a2c12d52581db8ad61
Ben Harrington <harringtonben@icloud.com>
2018-02-20 21:19:37 -0600

    refactored code to utilize using statement

--- ADOExample/Program.cs
+++ ADOExample/Program.cs
@@ -11,27 +11,27 @@ namespace ADOExample
     {
         static void Main(string[] args)
         {
-            var connection = new SqlConnection("Server=(local);Database=Chinook;Trusted_Connection=True;");
-            var cmd = connection.CreateCommand();
 
-            cmd.CommandText = @"select customerid, firstname, lastname, supportrepid from customer
-                                where country = 'Brazil'";
-
-            connection.Open();
-
-            var reader = cmd.ExecuteReader();
-
-            while (reader.Read())
+            using (var connection = new SqlConnection("Server=(local);Database=Chinook;Trusted_Connection=True;"))
             {
-                var customerId = reader.GetInt32(0);
-                var firstname = reader.GetSqlString(1);
-                var lastname = reader.GetSqlString(2);
-                var supportRepId = reader.GetInt32(3);
+                connection.Open();
 
-                Console.WriteLine($"{customerId} {firstname} {lastname} {supportRepId}");
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"select customerid, firstname, lastname, supportrepid from customer
+                                where country = 'Brazil'";
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var customerId = reader.GetInt32(0);
+                    var firstname = reader.GetSqlString(1);
+                    var lastname = reader.GetSqlString(2);
+                    var supportRepId = reader.GetInt32(3);
+
+                    Console.WriteLine($"{customerId} {firstname} {lastname} {supportRepId}");
+                }
             }
-
-            connection.Close();
+               
             Console.ReadLine();
         }
     }
