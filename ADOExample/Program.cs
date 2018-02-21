ee5a432c3b9d56f80af9086be1d16211cd974d5e
Ben Harrington <harringtonben@icloud.com>
2018-02-20 20:46:25 -0600

    initial commit

--- ADOExample/Program.cs
+++ ADOExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace ADOExample
     {
         static void Main(string[] args)
         {
+            var connection = new SqlConnection("Server=(local);Database=Chinook;Trusted_Connection=True;");
+            var cmd = connection.CreateCommand();
+
+            cmd.CommandText = @"select count(*) as [Total Invoices], DATENAME(yyyy, InvoiceDate) as Year from Invoice
+                                where InvoiceDate like '%2009%'
+                                or InvoiceDate like '%2011%'
+                                group by DATENAME(yyyy, InvoiceDate)";
+
+            connection.Open();
         }
     }
 }
