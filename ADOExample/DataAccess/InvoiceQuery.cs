d3c7fae4696c81223b19c6f85ec81ec8c65d45c5
Ben Harrington <harringtonben@icloud.com>
2018-02-24 11:02:16 -0600

    added data access layer

---
+++ ADOExample/DataAccess/InvoiceQuery.cs
@@ -0,0 +1,57 @@
+﻿using ADOExample.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOExample.DataAccess
+{
+    class InvoiceQuery
+    {
+        public List<Invoice> GetInvoiceByTrackFirstLetter(string firstCharacter)
+        {
+            using (var connection = new SqlConnection("Server=(local);Database=Chinook;Trusted_Connection=True;"))
+            {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = $@"select  i.*
+                                from invoice i
+	                                join InvoiceLine x on x.InvoiceId = i.InvoiceId
+                                where exists (select TrackId from Track where Name like @firstLetter and TrackId = x.TrackId)";
+
+                var firstLetter = new SqlParameter("@firstLetter", SqlDbType.NVarChar);
+                firstLetter.Value = firstCharacter;
+                cmd.Parameters.Add(firstLetter);
+
+                var reader = cmd.ExecuteReader();
+
+                var invoices = new List<Invoice>();
+
+                while (reader.Read())
+                {
+                    var invoice = new Invoice
+                    {
+                        InvoiceId = int.Parse(reader["InvoiceId"].ToString()),
+                        CustomerId = int.Parse(reader["CustomerId"].ToString()),
+                        InvoiceDate = DateTime.Parse(reader["InvoiceDate"].ToString()),
+                        BillingAddress = reader["BillingAddress"].ToString(),
+                        BillingCity = reader["BillingCity"].ToString(),
+                        BillingState = reader["BillingState"].ToString(),
+                        BillingCountry = reader["BillingCountry"].ToString(),
+                        BillingPostalCode = reader["BillingPostalCode"].ToString(),
+                        Total = double.Parse(reader["Total"].ToString())
+                    };
+
+                    invoices.Add(invoice);
+                }
+
+                return invoices;
+            }
+        }
+    }
+
+    
+}
--- ADOExample/Program.cs
+++ ADOExample/Program.cs
@@ -12,45 +12,14 @@ namespace ADOExample
     {
         static void Main(string[] args)
         {
-            var run = true;
-            while (run)
-            {
 
-                Console.WriteLine("What country would you like to see customers from? Type quit to stop the application");
-                var country = Console.ReadLine();
+            Console.WriteLine("What country would you like to see customers from?");
+            var firstletter = Console.ReadLine();
 
-                if (country == "quit")
-                {
-                    run = false;
-                    continue;
-                }
 
-                using (var connection = new SqlConnection("Server=(local);Database=Chinook;Trusted_Connection=True;"))
-                {
-                    connection.Open();
-                    var cmd = connection.CreateCommand();
-                    cmd.CommandText = @"select customerid, firstname, lastname, supportrepid from customer
-                                where country like @country";
 
-                    var countryInput = new SqlParameter("@country", SqlDbType.NVarChar);
-                    countryInput.Value = country;
-                    cmd.Parameters.Add(countryInput);
+            Console.ReadLine();
 
-                    var reader = cmd.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        var customerId = reader.GetInt32(0);
-                        var firstname = reader.GetSqlString(1);
-                        var lastname = reader.GetSqlString(2);
-                        var supportRepId = reader.GetInt32(3);
-
-                        Console.WriteLine($"{customerId} {firstname} {lastname} {supportRepId}");
-                    }
-
-                }
-            }
-               
         }
     }
 }
